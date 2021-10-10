eb4ea4997e300ca803c058b89675c93e8e5d5f3a
Agustín Covarrubias <agucova@uc.cl>
2021-10-10 02:08:44 -0300

    implement feedback by @SaschaMann

--- julia/rotational-cipher/rotational-cipher.jl
+++ julia/rotational-cipher/rotational-cipher.jl
@@ -1,15 +1,10 @@
 function rotate(k::Integer, char::AbstractChar)
-    if ! (0 <= k <= 26)
-        throw(DomainError("Key out of bounds (0 <= k <= 26)."))
-    end
+    0 <= k <= 26 || throw(DomainError("Key out of bounds (0 <= k <= 26)."))
     # We don't modify values outside of strictly alpha ASCII
-    if isascii(char) && isletter(char)
-        # Implements the wrap-around behaviour with char arithmetic
-        if isuppercase(char)
-            return Char((codepoint(char) + k - 65) % 26 + 65)
-        else
-            return Char((codepoint(char) + k - 97) % 26 + 97)
-        end
+    if char in 'A':'Z'
+        return 'A' + (char + k - 'A') % 26
+    elseif char in 'a':'z'
+        return 'a' + (char + k - 'a') % 26
     else
         return char
     end
@@ -17,13 +12,7 @@ end
 
 function rotate(k::Integer, plaintext::AbstractString)
     # Multiple dispatch really makes everything cleaner!
-    ciphertext = ""
-    for char in plaintext
-        # Wanted to use the dot operator for this, 
-        # but apparently it passes strings instead of chars.
-        ciphertext *= rotate(k, char)
-    end
-    ciphertext
+    map(x -> rotate(k, x), plaintext)
 end
 
 
